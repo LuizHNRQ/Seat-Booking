e7b8d2b78e19ae23679bd365c5c471f129b33d2f
Luiz Henrique Barros Teixeira <luizhbtluiz@gmail.com>
2020-04-14 11:34:40 -0300

    Added selected seats and total

--- script.js
+++ script.js
@@ -2,14 +2,35 @@ const container = document.querySelector(".container");
 const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.querySelector("#count");
 const total = document.querySelector("#total");
-const movieSelect = document.getElementById('movie')
-const ticketPrice = movieSelect.value;
+const movieSelect = document.getElementById("movie");
+let ticketPrice = +movieSelect.value;
 
+//Functions
 
-console.log(ticketPrice);
+function updateSlectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = `${selectedSeatsCount}`;
+  total.innerText = `${selectedSeatsCount * ticketPrice}`;
+}
+
+//select movie event listner
+
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  updateSlectedCount();
+});
+
+//Event Listner
 
 container.addEventListener("click", (e) => {
-  seats.forEach((seat) => {
-    console.log(seat);
-  });
+  const seat = e.target;
+
+  if (!seat.classList.contains("occupied") && seat.classList.contains("seat")) {
+    seat.classList.toggle("selected");
+    updateSlectedCount();
+  } else {
+    console.log("oi");
+  }
 });
